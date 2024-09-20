700933f4cce7076f2aee57d81e260558fe7922bc
iamoracle <38940247+iamoracle@users.noreply.github.com>
2024-09-20 16:26:25 +0100

    fixed bug

--- test/getDate.test.js
+++ test/getDate.test.js
@@ -5,6 +5,6 @@ describe("Testing if date works", function () {
   it("should return a date", function () {
     const date = getDate();
 
-    assert.equal(typeof date, "number");
+    assert.equal(typeof date, "string");
   });
 });
